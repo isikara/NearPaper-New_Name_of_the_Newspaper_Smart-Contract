fb9a5e0322c434edf3d50aa4a9363ee99d4a2c5d
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 11:02:43 +0300

    Update 2.healthCheck.sh

--- scripts/2.healthCheck.sh
+++ scripts/2.healthCheck.sh
@@ -10,7 +10,7 @@ set -e
 [ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
 
 [ -z "$YOUR_ACC" ] echo Please \export your account id to the \$YOUR_ACC variable!
-[ -z "$YOUR_ACC" ] echo 'export YOUR_ACC="YOUR ACCOUNT NAME'
+[ -z "$YOUR_ACC" ] echo 'export YOUR_ACC=YOUR ACCOUNT NAME'
 
 echo ---------------------------------------------------------
 echo "Step 1: Call 'call' functions on the contract"

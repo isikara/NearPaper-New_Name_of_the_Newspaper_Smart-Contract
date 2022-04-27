2cffa614747fc5954fa84f184f912c55103e8263
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 11:43:39 +0300

    Update 5.bring-news.sh

--- scripts/5.bring-news.sh
+++ scripts/5.bring-news.sh
@@ -6,16 +6,16 @@ set -e
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
 [ -z "$ID" ] && echo "Missing \$ID environment variable"
 
-[ -z "$CONTRACT" ] && echo Please \export your account id to the \$CONTRACT variable!
-[ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
+[ -z "$CONTRACT" ] && echo "Please \export your account id to the \$CONTRACT variable!"
+[ -z "$CONTRACT" ] && echo "export CONTRACT=__new_contract_account_id__"
 
-[ -z "$ID" ] echo Please \export the id of the news to the \$ID variable!
-[ -z "$ID" ] echo 'export ID=__id_of_the_news__'
+[ -z "$ID" ] && echo "Please \export the id of the news to the \$ID variable!"
+[ -z "$ID" ] && echo "export ID=__id_of_the_news__"
 
 echo ---------------------------------------------------------
 echo "Step 1: Call 'bringNews' functions on the contract"
 echo ---------------------------------------------------------
-echo "After all variables are exported, please use below function. 
+echo "After all variables are exported, please use below function."
 echo "near view $CONTRACT bringNews '{"id" : $ID}'"
 echo 
 echo

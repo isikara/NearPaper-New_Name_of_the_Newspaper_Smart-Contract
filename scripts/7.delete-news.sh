02a60144a445aebddb5433fa398021c67cf6611a
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 11:42:57 +0300

    Update 7.delete-news.sh

--- scripts/7.delete-news.sh
+++ scripts/7.delete-news.sh
@@ -9,19 +9,19 @@ set -e
 [ -z "$MY_ACC" ] && echo "Missing \$MY_ACC environment variable"
 [ -z "$ID" ] && echo "Missing \$ID environment variable"
 
-[ -z "$CONTRACT" ] && echo Please \export your account id to the \$CONTRACT variable!
-[ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
+[ -z "$CONTRACT" ] && echo "Please \export your account id to the \$CONTRACT variable!"
+[ -z "$CONTRACT" ] && echo "export CONTRACT=__new_contract_account_id__"
 
-[ -z "$MY_ACC" ] echo Please \export your account id to the \$MY_ACC variable!
-[ -z "$MY_ACC" ] echo 'export MY_ACC=__your_account_name__'
+[ -z "$MY_ACC" ] && echo "Please \export your account id to the \$MY_ACC variable!"
+[ -z "$MY_ACC" ] && echo "export MY_ACC=__your_account_name__"
 
-[ -z "$ID" ] echo Please \export your account id to the \$ID variable!
-[ -z "$ID" ] echo 'export ID=__id_of_the_news__'
+[ -z "$ID" ] && echo "Please \export your account id to the \$ID variable!"
+[ -z "$ID" ] && echo "export ID=__id_of_the_news__"
 
 echo ---------------------------------------------------------
 echo "Step 1: Call 'deleteNews' functions on the contract"
 echo ---------------------------------------------------------
-echo "After all variables are exported, please use below function. 
+echo "After all variables are exported, please use below function." 
 echo "near call $CONTRACT deleteNews '{"id" : $ID}' --accountId $MY_ACC"
 echo 
 echo

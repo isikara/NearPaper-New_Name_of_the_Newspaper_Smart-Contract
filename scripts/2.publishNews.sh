5eeac5b2188282c8345e9ebfaa6dc9bd59c14f1d
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 10:53:10 +0300

    Create 2.publishNews.sh

---
+++ scripts/2.publishNews.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# exit on first error after this point to avoid redeploying with successful build
+set -e
+
+[ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
+[ -z "$YOUR_ACC" ] && echo "Missing \$YOUR_ACC environment variable"
+
+[ -z "$CONTRACT" ] && echo Please \export your account id to the \$CONTRACT variable!
+[ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
+
+[ -z "$YOUR_ACC" ] echo Please \export your account id to the \$YOUR_ACC variable!
+[ -z "$YOUR_ACC" ] echo 'export YOUR_ACC="YOUR ACCOUNT NAME'
+
+echo ---------------------------------------------------------
+echo "Step 1: Call 'publishNews' functions on the contract"
+echo ---------------------------------------------------------
+echo 
+echo "near call $CONTRACT publishNews '{"text" : "<Your-News>"}' --accountId $YOUR_ACC"
+echo 
+
+echo
+exit 0

46afcc0415c40151076b5a06541e4a572f5c386d
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 11:44:48 +0300

    Update 3.publish-news.sh

--- scripts/3.publish-news.sh
+++ scripts/3.publish-news.sh
@@ -7,19 +7,19 @@ set -e
 [ -z "$MY_ACC" ] && echo "Missing \$MY_ACC environment variable"
 [ -z "$NEWS" ] && echo "Missing \$NEWS environment variable"
 
-[ -z "$CONTRACT" ] && echo Please \export your account id to the \$CONTRACT variable!
-[ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
+[ -z "$CONTRACT" ] && echo "Please \export your account id to the \$CONTRACT variable!"
+[ -z "$CONTRACT" ] && echo "export CONTRACT=__new_contract_account_id__"
 
-[ -z "$MY_ACC" ] echo Please \export your account id to the \$MY_ACC variable!
-[ -z "$MY_ACC" ] echo 'export MY_ACC=__your_account_name__'
+[ -z "$MY_ACC" ] && echo "Please \export your account id to the \$MY_ACC variable!"
+[ -z "$MY_ACC" ] && echo "export MY_ACC=__your_account_name__"
 
-[ -z "$NEWS" ] echo Please \export your account id to the \$NEWS variable!
-[ -z "$NEWS" ] echo 'export NEWS=__your_news__'
+[ -z "$NEWS" ] && echo "Please \export your account id to the \$NEWS variable!"
+[ -z "$NEWS" ] && echo "export NEWS=__your_news__"
 
 echo ---------------------------------------------------------
 echo "Step 1: Call 'publishNews' functions on the contract"
 echo ---------------------------------------------------------
-echo "After all variables are exported, please use below function. 
+echo "After all variables are exported, please use below function."
 echo "near call $CONTRACT publishNews '{"text" : $NEWS}' --accountId $MY_ACC"
 echo 
 

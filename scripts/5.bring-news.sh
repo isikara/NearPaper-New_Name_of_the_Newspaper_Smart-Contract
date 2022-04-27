9b67bafd6bcded65643fb5f3be41e0a3d58a1e47
Ali <36282023+isikara@users.noreply.github.com>
2022-04-27 11:09:10 +0300

    Update and rename 5.bringNews.sh to 5.bring-news.sh

--- scripts/5.bring-news.sh
+++ scripts/5.bring-news.sh
@@ -4,14 +4,10 @@
 set -e
 
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
-[ -z "$YOUR_ACC" ] && echo "Missing \$YOUR_ACC environment variable"
 [ -z "$ID" ] && echo "Missing \$ID environment variable"
 
 [ -z "$CONTRACT" ] && echo Please \export your account id to the \$CONTRACT variable!
 [ -z "$CONTRACT" ] && echo 'export CONTRACT=__new_contract_account_id__'
-
-[ -z "$YOUR_ACC" ] echo Please \export your account id to the \$YOUR_ACC variable!
-[ -z "$YOUR_ACC" ] echo 'export YOUR_ACC=__your_account_name__'
 
 [ -z "$ID" ] echo Please \export the id of the news to the \$ID variable!
 [ -z "$ID" ] echo 'export ID=__id_of_the_news__'

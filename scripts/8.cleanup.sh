700ce1f010d6c3dc7fbe4d873d84cf98a4ffa1b9
Ali <36282023+isikara@users.noreply.github.com>
2022-04-28 10:57:57 +0500

    Update 8.cleanup.sh

--- scripts/8.cleanup.sh
+++ scripts/8.cleanup.sh
@@ -10,17 +10,15 @@ echo ---------------------------------------------------------
 echo
 
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable" && exit 1
-[ -z "$BENEFICIARY" ] && echo "Missing \$BENEFICIARY environment variable" && exit 1
 [ -z "$CONTRACT" ] || echo "Found it! \$CONTRACT is set to [ $CONTRACT ]"
-[ -z "$BENEFICIARY" ] || echo "Found it! \$BENEFICIARY is set to [ $BENEFICIARY ]"
 
 echo
 echo
 echo ---------------------------------------------------------
-echo "Step 1: Delete $CONTRACT, setting $BENEFICIARY as beneficiary"
+echo "Step 1: Delete $CONTRACT"
 echo ---------------------------------------------------------
 echo
-near delete $CONTRACT $BENEFICIARY
+near delete $CONTRACT
 
 echo
 echo ---------------------------------------------------------

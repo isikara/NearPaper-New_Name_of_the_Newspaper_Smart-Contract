383998de1f3899a0682d0f128ee757a21c4579fb
Ali <36282023+isikara@users.noreply.github.com>
2022-04-28 10:56:36 +0500

    Update 7.send-gratitude.sh

--- scripts/7.send-gratitude.sh
+++ scripts/7.send-gratitude.sh
@@ -24,7 +24,7 @@ echo ---------------------------------------------------------
 echo "Step 1: Call 'sendGratitude' functions on the contract"
 echo ---------------------------------------------------------
 echo "After all variables are exported, please use below function. 
-echo "near call $CONTRACT sendGratitude '{"newsman" : $DEST_ACC}' --amount $AMOUNT --accountId $MY_ACC"
+echo "near call $CONTRACT sendGratitude '{"'"newsman"'" : "'"$DEST_ACC"'"}' --amount $AMOUNT --accountId $MY_ACC"
 echo 
 echo
 exit 0
